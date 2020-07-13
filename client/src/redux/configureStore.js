56f9f5b4d9fc0928a9ffd3f8dd26caf6469b64a1
Maxime Druart <maxime.druart@hetic.net>
2020-07-13 12:55:44 +0200

    :rocket: no other way but to commit keys for development process

--- client/src/redux/configureStore.js
+++ client/src/redux/configureStore.js
@@ -1,14 +1,14 @@
 import { createStore } from "redux"
 import rootReducer from "./reducers"
-import { applyMiddleware, compose } from "redux"
-// import { applyMiddleware } from "redux"
+// import { applyMiddleware, compose } from "redux"
+import { applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 
 // redux with redux devtools
 
-export default createStore(
-  rootReducer,
-  compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-)
+// export default createStore(
+//   rootReducer,
+//   compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// )
 
-// export default createStore(rootReducer, applyMiddleware(thunk))
+export default createStore(rootReducer, applyMiddleware(thunk))
